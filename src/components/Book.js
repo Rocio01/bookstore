bdeb102649822cd97a607f5c9367cccc6851df02
Zulma Rocio <zrmartinezg@gmail.com>
2021-11-01 18:35:33 -0500

    Add Book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,68 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Book(props) {
+const Book = (props) => {
+  const {
+    category, title, author, chapter, completed, id,
+  } = props;
   return (
-    <div />
+
+    <>
+      <div className="Book-info">
+        <ul>
+          <li className="category">
+            {category}
+          </li>
+          <li className="title">
+            {title}
+          </li>
+          <li className="author">
+            {author}
+          </li>
+          <li>
+            <button type="button" value={id}>Comments</button>
+            {' | '}
+            <button type="button" value={id}>Remove</button>
+            {' | '}
+            <button type="button" value={id}>Edit</button>
+          </li>
+        </ul>
+      </div>
+      <div className="progress">
+        <h4>
+          {' '}
+          {completed}
+          {' '}
+          % Completed
+        </h4>
+
+      </div>
+      <div>
+        <ul>
+          <li>
+            CURRENT CHAPTER
+          </li>
+          <li>
+            Chapter
+            {' '}
+            {chapter}
+          </li>
+          <li>
+            <button type="button" value={id}>UPDATE PROGRESS</button>
+          </li>
+        </ul>
+      </div>
+    </>
   );
-}
+};
+
+Book.propTypes = {
+  id: PropTypes.number.isRequired,
+  category: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  chapter: PropTypes.number.isRequired,
+  completed: PropTypes.number.isRequired,
+};
+
+export default Book;
