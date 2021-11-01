9deeeafd640c97fe01afe789e4871f93493ec3cc
Zulma Rocio <zrmartinezg@gmail.com>
2021-11-01 17:36:17 -0500

    Add Books container component

---
+++ src/components/BooksContainer.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+ const BooksContainer = () => {
+    const books: [
+      {
+        id: 1
+        category: "Fantasy",
+        title: "Harry Potter and the Sorcerer’s Stone",
+        author: "J.K. Rowling",
+        chapter: 1,
+        completed: 50,
+      },
+      {
+        id: 2
+        category: "Fantasy",
+        title: "Harry Potter and the Chamber of Secrets",
+        author: "J.K. Rowling",
+        chapter: 1,
+        completed: 80,
+      },
+      {
+        id: 3
+        category: "Fantasy",
+        title: "Harry Potter and the Prisoner of Azkaban",
+        author: "J.K. Rowling",
+        chapter: 1,
+        completed: 60,
+      },
+      
+    ]
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BooksContainer
