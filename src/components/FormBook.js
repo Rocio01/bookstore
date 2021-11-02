a5b24fc1a2f45f1f1d1ea0abf1189cb63ee82f35
Zulma Rocio <zrmartinezg@gmail.com>
2021-11-01 22:51:52 -0500

    Add author input in form component

--- src/components/FormBook.js
+++ src/components/FormBook.js
@@ -5,12 +5,18 @@ const FormBook = () => (
     <h4>
       ADD NEW BOOK
     </h4>
-    <form className="row gy-2 gx-3 align-items-cente">
+    <form className="row gy-2 gx-3 align-items-center">
       <div className="col-auto">
-        <div className="input-group">
+        <div className="input-group book-title">
           <input type="text" className="form-control" id="title" placeholder="Book title" />
         </div>
       </div>
+      <div className="col-auto">
+        <div className="input-group book-author">
+          <input type="text" className="form-control" id="author" placeholder="Author" />
+        </div>
+      </div>
+
       <div className="col-auto">
         <select className="form-control" id="sel1" placeholder="category">
           <option>Action</option>
