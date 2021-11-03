2e9eae903e5b0b4964335b3b2850991c1adbf8be
Zulma Rocio <zrmartinezg@gmail.com>
2021-11-03 17:56:49 -0500

    Add book store identifier, url, axios

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,7 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const BOOK_ID = 'om3QgaPKeSFviyizCbfq';
+const URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi';
 
 const initialState = [];
 
